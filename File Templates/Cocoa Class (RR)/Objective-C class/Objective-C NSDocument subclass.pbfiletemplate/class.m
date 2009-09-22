b92bcb6975c6b5c169d239662783fed3bb36699d
Roy Ratcliffe <roy@pioneeringsoftware.co.uk>
2009-09-22 07:01:46 +0100

    Tidied up the NSDocument and NSWindowController subclass templates

--- File Templates/Cocoa Class (RR)/Objective-C class/Objective-C NSDocument subclass.pbfiletemplate/class.m
+++ File Templates/Cocoa Class (RR)/Objective-C class/Objective-C NSDocument subclass.pbfiletemplate/class.m
@@ -26,31 +26,27 @@
 «OPTIONALHEADERIMPORTLINE»
 @implementation «FILEBASENAMEASIDENTIFIER»
 
-- (NSString *)windowNibName {
-    // Implement this to return a nib to load OR implement -makeWindowControllers to manually create your controllers.
-    return @"«FILEBASENAMEASIDENTIFIER»";
+//------------------------------------------------------------------------------
+#pragma mark                                          Simple Reading and Writing
+//------------------------------------------------------------------------------
+
+- (BOOL)readFromData:(NSData *)data ofType:(NSString *)typeName error:(NSError **)outError
+{
+	return YES;
 }
 
 - (NSData *)dataOfType:(NSString *)typeName error:(NSError **)outError
 {
-    // Insert code here to write your document to data of the specified type. If the given outError != NULL, ensure that you set *outError when returning nil.
-
-    // You can also choose to override -fileWrapperOfType:error:, -writeToURL:ofType:error:, or -writeToURL:ofType:forSaveOperation:originalContentsURL:error: instead.
-
-    // For applications targeted for Panther or earlier systems, you should use the deprecated API -dataRepresentationOfType:. In this case you can also choose to override -fileWrapperRepresentationOfType: or -writeToFile:ofType: instead.
-
-    return nil;
+	return nil;
 }
 
-- (BOOL)readFromData:(NSData *)data ofType:(NSString *)typeName error:(NSError **)outError
-{
-    // Insert code here to read your document from the given data of the specified type.  If the given outError != NULL, ensure that you set *outError when returning NO.
+//------------------------------------------------------------------------------
+#pragma mark                                      Windows and Window Controllers
+//------------------------------------------------------------------------------
 
-    // You can also choose to override -readFromFileWrapper:ofType:error: or -readFromURL:ofType:error: instead. 
-    
-    // For applications targeted for Panther or earlier systems, you should use the deprecated API -loadDataRepresentation:ofType. In this case you can also choose to override -readFromFile:ofType: or -loadFileWrapperRepresentation:ofType: instead.
-    
-    return YES;
+- (NSString *)windowNibName
+{
+	return @"«FILEBASENAMEASIDENTIFIER»";
 }
 
 @end
