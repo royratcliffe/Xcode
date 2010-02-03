a495771e5f09f0a40d5dd50bb8d32384fbf7876d
Roy Ratcliffe <roy@pioneeringsoftware.co.uk>
2010-02-03 13:35:43 +0000

    Cocoa Touch NSObject and UIView sub-classes customised

--- File Templates/Cocoa Touch Class (RR)/Objective-C class/UIView subclass.pbfiletemplate/class.m
+++ File Templates/Cocoa Touch Class (RR)/Objective-C class/UIView subclass.pbfiletemplate/class.m
@@ -1,32 +1,54 @@
+// «PROJECTNAME» «FILENAME»
 //
-//  «FILENAME»
-//  «PROJECTNAME»
+// Copyright © «YEAR», «FULLUSERNAME», «ORGANIZATIONNAME»
+// All rights reserved
 //
-//  Created by «FULLUSERNAME» on «DATE».
-//  Copyright «YEAR» «ORGANIZATIONNAME». All rights reserved.
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
 //
+//	The above copyright notice and this permission notice shall be included in
+//	all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS," WITHOUT WARRANTY OF ANY KIND, EITHER
+// EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO
+// EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+// OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+//
+//------------------------------------------------------------------------------
 
 «OPTIONALHEADERIMPORTLINE»
-
 @implementation «FILEBASENAMEASIDENTIFIER»
 
-
-- (id)initWithFrame:(CGRect)frame {
-    if (self = [super initWithFrame:frame]) {
-        // Initialization code
-    }
-    return self;
+- (id)initWithFrame:(CGRect)frame
+{
+	self = [super initWithFrame:frame];
+	if (self)
+	{
+		<#statements#>
+	}
+	return self;
 }
 
-
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+- (void)dealloc
+{
+	<#deallocations#>
+	[super dealloc];
 }
 
+//------------------------------------------------------------------------------
+#pragma mark                                                   UI View Rendering
+//------------------------------------------------------------------------------
 
-- (void)dealloc {
-    [super dealloc];
+- (void)drawRect:(CGRect)rect
+{
+	
 }
-
 
 @end
