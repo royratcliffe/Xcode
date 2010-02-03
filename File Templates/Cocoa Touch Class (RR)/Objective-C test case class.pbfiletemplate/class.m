9e5ea7856e96e432d1649a240a31c9d869f47caf
Roy Ratcliffe <roy@pioneeringsoftware.co.uk>
2010-02-03 13:26:49 +0000

    Cocoa Touch templates
    Duplicated from /Developer/Platforms/iPhoneOS.platform/Developer/Library/Xcode/File Templates/Cocoa Touch Class

---
+++ File Templates/Cocoa Touch Class (RR)/Objective-C test case class.pbfiletemplate/class.m
@@ -0,0 +1,34 @@
+//
+//  «FILENAME»
+//  «PROJECTNAME»
+//
+//  Created by «FULLUSERNAME» on «DATE».
+//  Copyright «YEAR» «ORGANIZATIONNAME». All rights reserved.
+//
+
+«OPTIONALHEADERIMPORTLINE»
+
+@implementation «FILEBASENAMEASIDENTIFIER»
+
+#if USE_APPLICATION_UNIT_TEST     // all code under test is in the iPhone Application
+
+- (void) testAppDelegate {
+    
+    id yourApplicationDelegate = [[UIApplication sharedApplication] delegate];
+    STAssertNotNil(yourApplicationDelegate, @"UIApplication failed to find the AppDelegate");
+    
+}
+
+#else                           // all code under test must be linked into the Unit Test bundle
+
+- (void) testMath {
+    
+    STAssertTrue((1+1)==2, @"Compiler isn't feeling well today :-(" );
+    
+}
+
+
+#endif
+
+
+@end
