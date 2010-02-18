a850b860c31262793396bf76aa965f342100f68a
Roy Ratcliffe <roy@pioneeringsoftware.co.uk>
2010-02-18 07:08:50 +0000

    Project Templates from iPhone SDK (version 3.1.2)

---
+++ Project Templates/iPhone OS Application/Window-based Application/Window-based Core Data Application/Classes/___PROJECTNAMEASIDENTIFIER___AppDelegate.h
@@ -0,0 +1,27 @@
+//
+//  ___PROJECTNAMEASIDENTIFIER___AppDelegate.h
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright ___ORGANIZATIONNAME___ ___YEAR___. All rights reserved.
+//
+
+@interface ___PROJECTNAMEASIDENTIFIER___AppDelegate : NSObject <UIApplicationDelegate> {
+
+    NSManagedObjectModel *managedObjectModel;
+    NSManagedObjectContext *managedObjectContext;	    
+    NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+    UIWindow *window;
+}
+
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+- (NSString *)applicationDocumentsDirectory;
+
+@end
+
